9985afc2c64fbf1285ef793379a6f3317615c5dc
Bruno <dlima.id@gmail.com>
2026-03-25 14:19:33 +0100

    feat: add onRegister prop and decouple registration from logout

--- visitas-virtuales/src/App.jsx
+++ visitas-virtuales/src/App.jsx
@@ -1,14 +1,12 @@
 import './assets/App.css';
 import MockAPITest from './components/dev/MockApiTest';
-import TopHeader from './components/TopHeader';
+
 
 function App() {
 	return (
-		<div className="h-screen w-full flex flex-col justify-center items-center space-y-6 bg-white">
-			<TopHeader ></TopHeader>
+		<div className="w-full flex flex-col justify-center items-center space-y-6 bg-white">
 			<MockAPITest />
 		</div>
 	);
 }
-
 export default App;
--- visitas-virtuales/src/components/TopHeader.jsx
+++ visitas-virtuales/src/components/TopHeader.jsx
@@ -1,67 +1,116 @@
-import { ChevronDown, LogOut, User } from 'lucide-react';
-import { useState } from 'react';
+import '../assets/App.css';
 
-export default function TopHeader({ userName = "Nombre de usuario", userImg = "https://unavatar.io/x/unknow", userEmail = "usuario@ejemplo.com", rol = "Administrador", onLogout = () => { } }) {
+import { ChevronDown, LogOut } from 'lucide-react';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+TopHeader.propTypes = {
+    isLog: PropTypes.bool.isRequired,
+    onLogin: PropTypes.func.isRequired,
+    onLogout: PropTypes.func.isRequired,
+    onRegister: PropTypes.func.isRequired,
+    userName: PropTypes.string.isRequired,
+    userImg: PropTypes.string.isRequired,
+    userEmail: PropTypes.string.isRequired,
+    role: PropTypes.string.isRequired,
+};
+
+/**
+ * TopHeader — Barra superior de la aplicación
+ *
+ * Uso:
+ * <TopHeader
+ *     isLog={isLog}                    > boolean, si el usuario está logueado
+ *     onLogin={handleLogin}            > función, se ejecuta al iniciar sesión
+ *     onLogout={handleLogout}          > función, se ejecuta al cerrar sesión
+ *     onRegister={handleOnRegister}    > función, se ejecuta al cerrar sesión
+ *     userName="Bruno García"          > nombre de usuario
+ *     userEmail="bruno@mail.com"       > email de usuario
+ *     userImg="https://..."            > avatar de usuario
+ *     role="Administrador"             > rol de usuario
+ * />
+ */
+
+// Datos de sesión del usuario y acción de salida para el navbar.
+export default function TopHeader({
+    isLog = false,
+    onLogin = () => { },
+    onLogout = () => { },
+    onRegister = () => {},
+    userName = "Usuario",
+    userEmail = "sin email",
+    userImg = "https://unavatar.io/x/unknow",
+    role = "Sin rol"
+}) {
+
     const [isOpen, setIsOpen] = useState(false);
 
     return (
         <header className="h-16 w-full bg-gray-100 border-b border-slate-100 flex items-center justify-end px-8 shadow-sm">
-            <div className="relative flex items-center gap-4">
+            {isLog ? (
+                <div className="relative flex items-center gap-4">
 
-                <div className="flex flex-col items-end leading-tight">
-                    <h2 className="text-sm font-bold text-slate-800">
-                        {userName}
-                    </h2>
-                    <span className={`text-[11px] font-semibold uppercase tracking-wider text-black/50`}>
-                        {rol}
-                    </span>
-                </div>
-
-                <button
-                aria-label='Menú de usuario'
-                aria-expanded={isOpen}
-                onClick={() => setIsOpen(!isOpen)}
-                className="group flex items-center gap-2 focus:outline-none"
-                >
-                    <div className="relative">
-                        <img
-                            src={userImg}
-                            alt="User Avatar"
-                            className="w-10 h-10 rounded-full object-cover border-2 border-slate-50 group-hover:border-blue-200 transition-all"
-                        />
-                        <div className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 border-2 border-white rounded-full"></div>
+                    <div className="flex flex-col items-end leading-tight">
+                        <h2 className="text-sm font-bold text-slate-800">
+                            {userName}
+                        </h2>
+                        <span className={`text-[11px] font-semibold uppercase tracking-wider text-black/50`}>
+                            {role}
+                        </span>
                     </div>
 
-                    <ChevronDown className={`w-4 h-4 transition-transform ${isOpen ? 'rotate-180' : ''}`} />
-                </button>
-
-                {isOpen && (
-                    <>
-                        <button
-                            className="fixed inset-0 z-10"
-                            onClick={() => setIsOpen(false)}
-                        ></button>
-                        <div className="absolute right-0 top-full mt-2 w-48 bg-white border border-slate-100 rounded-xl shadow-xl z-20 overflow-hidden animate-in fade-in zoom-in duration-200">
-                            <div className="px-4 py-3 border-b border-slate-50 bg-slate-50/50">
-                                <p className="text-xs text-slate-500 font-medium">Conectado como</p>
-                                <p className="text-sm font-bold text-slate-700 truncate">{userEmail}</p>
-                            </div>
-
-                            <button className="w-full text-left px-4 py-2.5 text-sm text-slate-600 hover:bg-slate-50 flex items-center gap-2 transition-colors">
-                                <User className="w-4 h-4" />
-                                Mi Perfil
-                            </button>
-
-                            <button
-                                onClick={() => { onLogout(); setIsOpen(false); }}
-                                className="w-full text-left px-4 py-2.5 text-sm text-red-500 hover:bg-red-50 font-semibold flex items-center gap-2 transition-colors">
-                                <LogOut className="w-4 h-4" />
-                                Cerrar sesión
-                            </button>
+                    <button
+                        onClick={() => setIsOpen(!isOpen)}
+                        className="group flex items-center gap-2 focus:outline-none">
+                        <div className="relative">
+                            <img
+                                src={userImg}
+                                alt="User Avatar"
+                                className="w-10 h-10 rounded-full object-cover border-2 border-slate-50 group-hover:border-blue-200 transition-all"
+                            />
+                            <div className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 border-2 border-white rounded-full"></div>
                         </div>
-                    </>
-                )}
-            </div>
-        </header >
+                        <ChevronDown className={`w-4 h-4 transition-transform ${isOpen ? 'rotate-180' : ''}`} />
+                    </button>
+
+                    {isOpen && (
+                        <>
+                            <button
+                                className="fixed inset-0 z-10"
+                                onClick={() => setIsOpen(false)}
+                            ></button>
+                            <div className="absolute right-0 top-full mt-2 w-48 bg-white border border-slate-100 rounded-xl shadow-xl z-20 overflow-hidden animate-in fade-in zoom-in duration-200">
+                                <div className="px-4 py-3 border-b border-slate-50 bg-slate-50/50">
+                                    <p className="text-xs text-slate-500 font-medium">Conectado como</p>
+                                    <p className="text-sm font-bold text-slate-700 truncate">{userEmail}</p>
+                                </div>
+
+                                <button
+                                    onClick={() => { onLogout(); setIsOpen(false); }}
+                                    className="w-full text-left px-4 py-2.5 text-sm text-red-500 hover:bg-red-50 font-semibold flex items-center gap-2 transition-colors">
+                                    <LogOut className="w-4 h-4" />
+                                    Cerrar sesión
+                                </button>
+                            </div>
+                        </>
+                    )}
+                </div>
+            ) : (
+                <div className="relative flex items-center gap-4">
+                    <div className="flex flex-row items-end leading-tight">
+                        <button
+                            onClick={onLogin}
+                            className="w-30 m-2 p-2 text-slate-500 border-b border-transparent hover:text-slate-800 hover:border-slate-800 transition-colors">
+                            Iniciar Sesión
+                        </button>
+                        <button
+                            onClick={onRegister}
+                            className="w-30 m-2 p-2 text-slate-500 border-b border-transparent hover:text-slate-800 hover:border-slate-800 transition-colors">
+                            Registrarte
+                        </button>
+                    </div>
+                </div>
+            )}
+        </header>
     );
 };
