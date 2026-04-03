5ffa442835249e544e7a2b93d7b2cb4af15be290
orlaa24ad <orlaa24ad@pm.me>
2026-04-03 17:16:30 +0200

    Arreglos pequeños detalles de visualizacion y personalizacion

--- src/components/dev/layout/Header.jsx
+++ src/components/dev/layout/Header.jsx
@@ -1,10 +1,10 @@
 export const Header = ({ onMenuClick }) => (
-  <header className="h-16 bg-blue-800 text-white flex items-center justify-between px-6 shadow-lg z-10">
+  <header className="h-16 bg-gray-300 text-white flex items-center justify-between px-6 shadow-lg z-10">
     <div className="flex items-center gap-2">
       {/* Botón de Hamburguesa: visible solo en móvil, activa el toggleSidebar */}
       <button 
         onClick={onMenuClick}
-        className="p-2 mr-2 text-white md:hidden hover:bg-gray-400 rounded-lg transition-colors"
+        className="p-2 mr-2 text-black md:hidden hover:bg-gray-400 rounded-lg transition-colors"
         aria-label="Abrir menú"
       >
         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -12,13 +12,12 @@ export const Header = ({ onMenuClick }) => (
         </svg>
       </button>
 
-      <div className="w-1 h-8 bg-blue-800 rounded-lg"></div>
-      <div className="w-8 h-8 bg-white rounded-lg" />
-      <span className="font-bold text-xl tracking-tight">Centro</span>
+      <div className="w-8 h-8 bg-black rounded-lg cursor-pointer" />
+      <span className="font-bold text-xl text-black tracking-tight cursor-pointer">Header</span>
     </div>
 
     <div className="p-4 border-t border-black">
-      <button className="w-full flex items-center gap-3 p-3 text-white hover:bg-slate-600 rounded-xl transition-all cursor-pointer">
+      <button className="w-full flex items-center gap-3 p-3 text-black hover:bg-blue-600 rounded-xl transition-all cursor-pointer">
         <span className="font-medium">Admin</span>
       </button>
     </div>
