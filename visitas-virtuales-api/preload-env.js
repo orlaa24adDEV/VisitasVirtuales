6b2a452fc013a7b03aed929b83d9de5681321b6e
pva0011 <pva0011@alu.medac.es>
2026-04-06 20:14:21 +0200

    chore(deploy): setup pm2 and app dockerization for prod

--- visitas-virtuales-api/preload-env.js
+++ visitas-virtuales-api/preload-env.js
@@ -1,4 +1,10 @@
 import dotenv from 'dotenv'
 import dotenvExpand from 'dotenv-expand'
-const env = dotenv.config({ path: process.env.ENV_PATH || '.env.dev' })
-dotenvExpand.expand(env)
+// Solo cargar un .env si no estamos en producción
+if (process.env.NODE_ENV !== 'production') {
+  const env = dotenv.config({ path: process.env.ENV_PATH || '.env.dev' })
+  dotenvExpand.expand(env)
+// En producción se asume que Docker proporcionará las variables de entorno
+} else {
+  dotenvExpand.expand({ parsed: process.env })
+}
