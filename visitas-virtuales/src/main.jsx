b775bfc9c74248541e53dea69ffe55e3f4be56c1
orlaa24ad <orlaa24ad@pm.me>
2026-04-08 12:26:58 +0200

    fixed: Issue with dashboard, onUnhandledRequest: warn

--- visitas-virtuales/src/main.jsx
+++ visitas-virtuales/src/main.jsx
@@ -9,7 +9,7 @@ async function enableMocking() {
 	if (import.meta.env.DEV) {
 		const { worker } = await import('./mocks/browser');
 		await worker.start({
-			onUnhandledRequest: 'bypass',
+			onUnhandledRequest: 'warn',
 			serviceWorker: {
 				url: '/mockServiceWorker.js',
 			},
