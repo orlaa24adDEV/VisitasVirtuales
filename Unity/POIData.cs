54c9563a119e7add0753fe592887c9045ca93ca4
fermiinbp03 <fbp0006@alu.medac.es>
2026-04-09 14:07:13 +0200

    Add POIData class for serializable data structure

---
+++ Unity/POIData.cs
@@ -0,0 +1,14 @@
+[System.Serializable]
+public class POIData
+{
+    public string p1;
+    public string p2;
+    public string p3;
+    public string p4;
+    public string p5;
+    public string p6;
+    public string p7;
+    public string p8;
+    public string p9;
+    public string p10;
+}
