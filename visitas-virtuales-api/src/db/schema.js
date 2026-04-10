5af6fe7ea3beacf530649e671536f06258456a28
Flor <florazul.fb@gmail.com>
2026-04-10 13:55:44 +0200

    feat: add poi_history table for POI change tracking

--- visitas-virtuales-api/src/db/schema.js
+++ visitas-virtuales-api/src/db/schema.js
@@ -1,3 +1,4 @@
+import { time } from 'drizzle-orm/mysql-core'
 import {
 	pgTable,
 	integer,
@@ -70,4 +71,18 @@ export const statsPois = pgTable('stats_pois', {
 	poi_id: integer('poi_id')
 		.notNull()
 		.references(() => pois.id, { onDelete: 'cascade' }),
+})
+
+//tabla de trazabilidad
+export const poiHistory = pgTable('poi_history', {
+	id: serial('id').primaryKey(),
+	poiId: integer('poi_id')
+		.notNull()
+		.references(() => pois.id, {onDelete: 'cascade'}),
+	userId: integer('user_id')
+		.notNull()
+		.references(() => users.id, { onDelete: 'cascade' }),
+	action: text('action').notNull(),
+	timestamp: timestamp('timestamp').notNull().defaultNow(),
+	details: jsonb('details'),
 })
