260dc3d72cc38471eb1993c0fec2ac09c49bfa90
pablodegalvez <pablo.degalvezpozo@gmail.com>
2026-04-13 10:26:58 +0200

    Conecto UnityViewers con la Built y corrijo un par de errores

--- visitas-virtuales/src/components/Crud.jsx
+++ visitas-virtuales/src/components/Crud.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
-import { useAuth } from '../context/AuthContext';
+//import { useAuth } from '../context/AuthContext';
+import { useAuth } from '@/hooks/useAuth';
 
 function Crud() {
     const { selectedCenter } = useAuth();
