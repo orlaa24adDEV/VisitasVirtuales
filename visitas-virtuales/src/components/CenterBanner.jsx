47a3b911845f15b40c627f944609c4ddace5a839
pva0011 <pva0011@alu.medac.es>
2026-04-17 00:40:45 +0200

    feat(ui): extract center banner to reusable component, add it to POI CRUD, remove center name from form

---
+++ visitas-virtuales/src/components/CenterBanner.jsx
@@ -0,0 +1,7 @@
+export default function CenterBanner({ centerName }) {
+  return (
+    <div className="flex justify-center min-h-12 items-center border border-blue-400 bg-blue-400 rounded-lg w-full mb-4">
+      <h1 className="w-full p-1 text-white text-center font-semibold">{centerName}</h1>
+    </div>
+  );
+}
