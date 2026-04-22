a52614875d88f15f49e8d0a6c897cb80d32f5ef7
Bruno <dlima.id@gmail.com>
2026-04-22 11:51:19 +0200

    feat: add Lading Page

--- visitas-virtuales/src/pages/LadingPage.jsx
+++ visitas-virtuales/src/pages/LadingPage.jsx
@@ -1,26 +1,38 @@
-function LadingPage() {
+import { Link } from "react-router-dom";
+
+const LandingPage = () => {
   return (
-    <div className="absolute w-full h-screen bg-gradient-to-r from-cyan-300 via-blue-500 to-purple-600 ">
-      <header className="flex flex-row w-full p-5 justify-between border">
-        <h1 className="w-40 p-1 text-3xl uppercase text-white font-bold ">
-          Proyecto360
+    <div className="flex flex-col h-full bg-white items-center justify-center">
+      <h1>LANDING PAGE</h1>
+      <header className="text-center mb-6">
+        <h1 className="text-4xl font-bold text-slate-800">
+          Bienvenido a Visitas Virtuales
         </h1>
-        <nav>
-          <button
-            type="text"
-            className="w-40 p-2 text-white bg-blue-400 border border-blue-400 rounded-lg shadow-md"
-          >
-            Iniciar Sesión
-          </button>
-        </nav>
+        <p className="text-slate-500 mt-2 text-lg">
+          Explora centros educativos de forma virtual
+        </p>
       </header>
-      <section>
-        <div>
-          <h1>Esto es el contenido de la lading... </h1>
-        </div>
+      <button className=" items-right px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">
+        <Link to="/login">Iniciar Sesión</Link>
+      </button>
+      <button className="items-right px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">
+        <Link to="/centros">Explorar Centros</Link>
+      </button>
+      <button className="items-right px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">
+        <a href="#information" className="scroll-smooth">
+          Saber mas
+        </a>
+      </button>
+      <section
+        id="information"
+        className="mt-20 w-full max-w-4xl px-6 py-10 bg-gray-50 rounded-lg shadow-md"
+      >
+        <h2 className="text-2xl font-bold text-slate-800 mb-4">
+          Sobre Nosotros
+        </h2>
+        <p className="text-slate-600">Nosotros</p>
       </section>
     </div>
   );
-}
-
-export default LadingPage;
+};
+export default LandingPage;
